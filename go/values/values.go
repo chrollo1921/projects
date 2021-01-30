b736345e028d5d62fd00cae894c168c2fecc4877
chrollo1921 <karthik.b1921@outlook.com>
2021-01-30 20:21:35 +0530

    Added go files and scala files

---
+++ go/values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Karthik" + "B")
+  fmt.Println("1+1= ", 1+1)
+  fmt.Println("7.30/3.30= ", 7.30/3.30)
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+
+}
